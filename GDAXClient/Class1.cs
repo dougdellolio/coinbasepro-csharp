04a8253f233f35bb62913a4485029606a8c8f66f
dougdellolio <doug.dellolio@gmail.com>
2017-11-22 21:47:30 -0500

    Add project files.

---
+++ GDAXClient/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDAXClient
+{
+    public class Class1
+    {
+    }
+}
