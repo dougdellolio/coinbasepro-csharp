de4ef2684a5d2bcbe0145ca0d9411c582a5f3263
Doug Dellolio <doug.dellolio@gmail.com>
2017-12-24 11:46:54 -0500

    [Feature] Fundings Service (#30)

---
+++ GDAXClient.Specs/Utilities/QueryBuilderSpecs.cs
@@ -0,0 +1,45 @@
+﻿using GDAXClient.Utilities;
+using Machine.Fakes;
+using Machine.Specifications;
+using System.Collections.Generic;
+
+namespace GDAXClient.Specs.Utilities
+{
+    [Subject("QueryBuilder")]
+    public class QueryBuilderSpecs : WithSubject<QueryBuilder>
+    {
+        static string result;
+
+        class when_requesting_with_multiple_parameters
+        {
+            Because of = () =>
+            {
+                result = Subject.BuildQuery(
+                    new KeyValuePair<string, string>("limit", "10"),
+                    new KeyValuePair<string, string>("status", "approved"),
+                    new KeyValuePair<string, string>("product_id", "eth-usd"));
+            };
+
+            It should_have_built_the_correct_query = () =>
+            {
+                result.ShouldEqual("?limit=10&status=approved&product_id=eth-usd");
+            };
+        }
+
+        class when_requesting_with_an_optional_parameter_and_empty_string_is_passed
+        {
+            Because of = () =>
+            {
+                result = Subject.BuildQuery(
+                    new KeyValuePair<string, string>("limit", "10"),
+                    new KeyValuePair<string, string>("status", "approved"),
+                    new KeyValuePair<string, string>("product_id", ""));
+            };
+
+            It should_have_built_the_correct_query = () =>
+            {
+                result.ShouldEqual("?limit=10&status=approved");
+            };
+        }
+    }
+}
---
+++ GDAXClient/Services/Fundings/Models/Funding.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GDAXClient.Services.Fundings.Models
+{
+    public class Funding
+    {
+        public Guid Id { get; set; }
+
+        public string Order_id { get; set; }
+
+        public string Profile_id { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime Created_at { get; set; }
+
+        public string Currency { get; set; }
+
+        public decimal Repaid_amount { get; set; }
+
+        public decimal Default_amount { get; set; }
+
+        public bool Repaid_default { get; set; }
+    }
+}
