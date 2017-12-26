cb317a6fd9cc5a16da99c23e9c797ac757630130
Devin Gelineau <keelog7331@gmail.com>
2017-12-26 17:38:58 -0500

    [Feature] Reports

---
+++ GDAXClient.Specs/JsonFixtures/Reports/ReportsResponseFixture.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDAXClient.Specs.JsonFixtures.Reports
+{
+    public class ReportsResponseFixture
+    {
+        public static string Create()
+        {
+            var json = @"
+    {
+        ""id"": ""0428b97b-bec1-429e-a94c-59232926778d"",
+        ""type"": ""fills"",
+        ""status"": ""pending"",
+        ""created_at"": ""2015-01-06T10:34:47.000Z"",
+        ""completed_at"": null,
+        ""expires_at"": ""2015-01-13T10:35:47.000Z"",
+        ""file_url"": null,
+        ""params"": {
+            ""start_date"": ""2014-11-01T00:00:00.000Z"",
+            ""end_date"": ""2014-11-30T23:59:59.000Z""
+        }
+    }";
+
+            return json;
+        }
+    }
+}
