0d9c6516f7092326b87bb2506523bb0e5b0444ea
Viletung <kevin.wilbrink@googlemail.com>
2018-03-30 21:09:56 +0200

    Removed unnecessary properties.

--- GDAXSharp/WebSocket/Models/Request/TickerChannel.cs
+++ GDAXSharp/WebSocket/Models/Request/TickerChannel.cs
@@ -10,32 +10,25 @@ namespace GDAXSharp.WebSocket.Models.Request
     public class TickerChannel
     {
         [JsonConverter(typeof(StringEnumConverter))]
-        [JsonProperty("type")]
         public ActionType Type { get; set; }
 
         [JsonProperty("product_ids", ItemConverterType = typeof(StringEnumConverter))]
         public List<ProductType> ProductIds { get; set; }
 
-        [JsonProperty("channels")]
         public List<Channel> Channels { get; set; }
 
-        [JsonProperty("signature")]
         public string Signature { get; set; }
 
-        [JsonProperty("key")]
         public string Key { get; set; }
 
-        [JsonProperty("passphrase")]
         public string Passphrase { get; set; }
 
-        [JsonProperty("timestamp")]
         public string Timestamp { get; set; }
     }
 
     public class Channel
     {
         [JsonConverter(typeof(StringEnumConverter))]
-        [JsonProperty("name")]
         public ChannelType Name { get; set; }
 
         [JsonProperty("product_ids", ItemConverterType = typeof(StringEnumConverter))]
