95255941b8220f59d4a81718f435d431b3ae1374
dougdellolio <doug.dellolio@gmail.com>
2018-06-26 19:59:06 -0400

    fix tests

---
+++ CoinbasePro/WebSocket/Models/Response/Change.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CoinbasePro.Services.Orders.Types;
+using CoinbasePro.Shared.Types;
+
+namespace CoinbasePro.WebSocket.Models.Response
+{
+    public class Change : BaseMessage
+    {
+        public Guid OrderId { get; set; }
+
+        public decimal NewSize { get; set; }
+
+        public decimal OldSize { get; set; }
+
+        public decimal OldFunds { get; set; }
+
+        public decimal NewFunds { get; set; }
+
+        public decimal Price { get; set; }
+
+        public OrderSide Side { get; set; }
+
+        public ProductType ProductId { get; set; }
+
+        public long Sequence { get; set; }
+
+        public DateTime Time { get; set; }
+    }
+}
