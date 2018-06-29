80633f0659420d4715d4c76cd5c0e588b950a78d
dougdellolio <doug.dellolio@gmail.com>
2018-06-29 14:41:03 -0400

    fix uris

--- CoinbasePro/Shared/ApiUris.cs
+++ CoinbasePro/Shared/ApiUris.cs
@@ -2,12 +2,12 @@
 {
     public class ApiUris
     {
-        public const string WebsocketUri = "wss://ws-feed.gdax.com";
+        public const string WebsocketUri = "wss://ws-feed.pro.coinbase.com";
 
         public const string WebsocketUriSandbox = "wss://ws-feed-public.sandbox.gdax.com";
 
         public const string ApiUri = "https://api.pro.coinbase.com";
 
-        public const string ApiUriSandbox = "https://api-public.sandbox.gdax.com";
+        public const string ApiUriSandbox = "https://api-public.sandbox.pro.coinbase.com";
     }
 }
