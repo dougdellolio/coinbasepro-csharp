4f1ce817cf9b10eb751841e591b469994c2a4696
dougdellolio <doug.dellolio@gmail.com>
2018-09-07 20:16:59 -0400

    [Feature] Add new GBP trading pairs

--- CoinbasePro/Shared/Types/ProductType.cs
+++ CoinbasePro/Shared/Types/ProductType.cs
@@ -16,23 +16,31 @@ namespace CoinbasePro.Shared.Types
         EthEur,
         [EnumMember(Value = "ETH-BTC")]
         EthBtc,
+        [EnumMember(Value = "ETH-GBP")]
+        EthGbp,
         [EnumMember(Value = "LTC-USD")]
         LtcUsd,
         [EnumMember(Value = "LTC-EUR")]
         LtcEur,
         [EnumMember(Value = "LTC-BTC")]
         LtcBtc,
+        [EnumMember(Value = "LTC-GBP")]
+        LtcGbp,
         [EnumMember(Value = "BCH-USD")]
         BchUsd,
         [EnumMember(Value = "BCH-EUR")]
         BchEur,
         [EnumMember(Value = "BCH-BTC")]
         BchBtc,
+        [EnumMember(Value = "BCH-GBP")]
+        BchGbp,
         [EnumMember(Value = "ETC-USD")]
         EtcUsd,
         [EnumMember(Value = "ETC-EUR")]
         EtcEur,
         [EnumMember(Value = "ETC-BTC")]
-        EtcBtc
+        EtcBtc,
+        [EnumMember(Value = "ETC-GBP")]
+        EtcGbp
     }
 }
