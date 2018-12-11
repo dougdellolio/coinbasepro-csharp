fec4eceef638a73740f0ab681675fee613b6645b
dougdellolio <doug.dellolio@gmail.com>
2018-12-10 21:23:58 -0500

    fix tests

--- CoinbasePro.Specs/JsonFixtures/Services/Reports/ReportStatusFixture.cs
+++ CoinbasePro.Specs/JsonFixtures/Services/Reports/ReportStatusFixture.cs
@@ -5,7 +5,7 @@
         public static string Create()
         {
             var json = @"
-{{
+{
     ""id"": ""0428b97b-bec1-429e-a94c-59232926778d"",
     ""type"": ""fills"",
     ""status"": ""creating"",
@@ -13,11 +13,11 @@
     ""completed_at"": undefined,
     ""expires_at"": ""2016-12-08T24:00:00Z"",
     ""file_url"": undefined,
-    ""params"": {{
+    ""params"": {
         ""start_date"": ""2016-12-08T24:00:00Z"",
         ""end_date"": ""2016-12-08T24:00:00Z""
-    }}
-}}";
+    }
+}";
 
             return json;
         }
