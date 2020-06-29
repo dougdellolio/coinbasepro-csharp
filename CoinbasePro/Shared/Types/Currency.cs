6359841467f3ae2dd9800ea831f3d000bb4d0a56
dougdellolio <doug.dellolio@gmail.com>
2020-06-29 08:52:50 -0400

    revert changes

---
+++ CoinbasePro/Shared/Types/Currency.cs
@@ -0,0 +1,40 @@
+﻿namespace CoinbasePro.Shared.Types
+{
+    public enum Currency
+    {
+        BAT,
+        BCH,
+        BTC,
+        CVC,
+        DAI,
+        DNT,
+        EOS,
+        ETC,
+        ETH,
+        EUR,
+        GBP,
+        GNT,
+        LOOM,
+        LTC,
+        MANA,
+        MKR,
+        REP,
+        USD,
+        USDC,
+        Unknown,
+        XLM,
+        XRP,
+        XTZ,
+        ZEC,
+        ZIL,
+        ZRX,
+        ALGO,
+        LINK,
+        DASH,
+        OXT,
+        ATOM,
+        OMG,
+        KNC,
+        COMP
+    }
+}
